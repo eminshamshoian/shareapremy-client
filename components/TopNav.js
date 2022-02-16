b7aeab2c2c0370ea9af440e41cee442e2b23d37a
Emin <eminshamshoian@gmail.com>
2022-02-16 11:40:12 -0800

    added active navigation tabs

--- components/TopNav.js
+++ components/TopNav.js
@@ -1,28 +1,41 @@
-import { Menu } from 'antd';
-import Link from 'next/link';
-import {
-  AppstoreOutlined,
-  LoginOutlined,
-  UserAddOutlined,
-} from '@ant-design/icons';
+import { useState, useEffect } from "react";
+import { Menu } from "antd";
+import Link from "next/link";
 
 // Menu Items
 const { Item } = Menu;
 
 const TopNav = () => {
+  // Current state of the selected navitem
+  const [current, setCurrent] = useState("");
+
+  // useEffect on client side only and set current state to the current window
+  useEffect(() => {
+    process.browser && setCurrent(window.location.pathname);
+  }, [process.browser && window.location.pathname]);
+
   return (
-    <Menu className='top-nav' mode='horizontal'>
-      <Item>
+    // Give each menu item a key and set current state to that navitem and selectedKeys to the current key array
+    <Menu className='top-nav' mode='horizontal' selectedKeys={[current]}>
+      <Item key='/' onClick={(e) => setCurrent(e.key)} className='nav-item'>
         <Link href='/'>
           <a className='typewriter menu-a-tag'>Home</a>
         </Link>
       </Item>
-      <Item>
+      <Item
+        key='/login'
+        onClick={(e) => setCurrent(e.key)}
+        className='nav-item'
+      >
         <Link href='/login'>
           <a className='typewriter menu-a-tag'>Login</a>
         </Link>
       </Item>
-      <Item className='nav-item'>
+      <Item
+        key='/register'
+        onClick={(e) => setCurrent(e.key)}
+        className='nav-item'
+      >
         <Link href='/register'>
           <a className='typewriter menu-a-tag'>Register</a>
         </Link>
--- pages/register.js
+++ pages/register.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { SyncOutlined } from "@ant-design/icons";
+import Link from "next/link";
 
 const Register = () => {
   // Create states
@@ -65,6 +66,12 @@ const Register = () => {
             {loading ? <SyncOutlined spin /> : "Submit"}
           </button>
         </form>
+        <p className='text-center p-3 text-dark'>
+          Already registered?{" "}
+          <Link href='/login'>
+            <a>Login</a>
+          </Link>
+        </p>
       </div>
     </>
   );
