b5c28b25996c7c98fc897a6ad61ad17d7e325bbe
Emin <eminshamshoian@gmail.com>
2022-02-16 11:24:44 -0800

    modified babel for errors and created dev proxy server

--- pages/register.js
+++ pages/register.js
@@ -1,13 +1,13 @@
-import { useState } from 'react';
-import axios from 'axios';
-import { toast } from 'react-toastify';
-import { SyncOutlined } from '@ant-design/icons';
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { SyncOutlined } from "@ant-design/icons";
 
 const Register = () => {
   // Create states
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
 
   // Function to handle the submitting of data
@@ -15,15 +15,12 @@ const Register = () => {
     e.preventDefault();
     try {
       setLoading(true);
-      const { data } = await axios.post(
-        `${process.env.NEXT_PUBLIC_API}/register`,
-        {
-          name,
-          email,
-          password,
-        }
-      );
-      toast.success('Registeration Completed! Please Login');
+      const { data } = await axios.post(`/api/register`, {
+        name,
+        email,
+        password,
+      });
+      toast.success("Registeration Completed! Please Login");
       setLoading(false);
     } catch (err) {
       toast.error(err.response.data);
@@ -65,7 +62,7 @@ const Register = () => {
             className='btn main-btn p-2'
             disabled={!name || !email || !password || loading}
           >
-            {loading ? <SyncOutlined spin /> : 'Submit'}
+            {loading ? <SyncOutlined spin /> : "Submit"}
           </button>
         </form>
       </div>
