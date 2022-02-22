b5c0e60f1b88e15aab98b354f9aa4c55211bb9fb
eminshamshoian <eminshamshoian@gmail.com>
2022-02-22 15:58:05 -0800

    added a footer and added a box shadow under icons in /about

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Link from 'next/link';
 import {
   UserAddOutlined,
   UploadOutlined,
@@ -6,7 +6,7 @@ import {
   MoneyCollectOutlined,
   BankOutlined,
   RiseOutlined,
-} from "@ant-design/icons";
+} from '@ant-design/icons';
 
 const Index = () => {
   return (
@@ -130,6 +130,105 @@ const Index = () => {
             </button>
           </div>
         </div>
+      </section>
+      <section id='footer' className='mt-5'>
+        <footer class='text-center text-lg-start bg-light text-muted'>
+          <section class='d-flex justify-content-center justify-content-lg-between p-4'>
+            <div>
+              <a href='' class='me-4 text-reset'>
+                <i class='fab fa-facebook-f'></i>
+              </a>
+              <a href='' class='me-4 text-reset'>
+                <i class='fab fa-twitter'></i>
+              </a>
+              <a href='' class='me-4 text-reset'>
+                <i class='fab fa-google'></i>
+              </a>
+              <a href='' class='me-4 text-reset'>
+                <i class='fab fa-instagram'></i>
+              </a>
+              <a href='' class='me-4 text-reset'>
+                <i class='fab fa-linkedin'></i>
+              </a>
+              <a href='' class='me-4 text-reset'>
+                <i class='fab fa-github'></i>
+              </a>
+            </div>
+          </section>
+          <section>
+            <div class='container text-center text-md-start'>
+              <div class='row'>
+                <div class='col-md-3 col-lg-4 col-xl-3 mx-auto mb-4'>
+                  <h6 class='text-uppercase fw-bold mb-4'>
+                    <i class='fas fa-gem'></i>ShareAPremy
+                  </h6>
+                  <p>
+                    Premium video sharing website. Create your video collections
+                    and share them while you earn.
+                  </p>
+                </div>
+                <div class='col-md-2 col-lg-2 col-xl-2 mx-auto mb-4'>
+                  <h6 class='text-uppercase fw-bold mb-4'>Products</h6>
+                  <p>
+                    <a href='#!' class='text-reset'>
+                      Video Sharing
+                    </a>
+                  </p>
+                  <p>
+                    <a href='#!' class='text-reset'>
+                      Account Management
+                    </a>
+                  </p>
+                  <p>
+                    <a href='#!' class='text-reset'>
+                      Stripe Integration
+                    </a>
+                  </p>
+                  <p>
+                    <a href='#!' class='text-reset'>
+                      Commission
+                    </a>
+                  </p>
+                </div>
+                <div class='col-md-3 col-lg-2 col-xl-2 mx-auto mb-4'>
+                  <h6 class='text-uppercase fw-bold mb-4'>Useful links</h6>
+                  <p>
+                    <a href='#!' class='text-reset'>
+                      Privacy Policy
+                    </a>
+                  </p>
+                  <p>
+                    <a href='#!' class='text-reset'>
+                      Refund Policy
+                    </a>
+                  </p>
+                  <p>
+                    <a href='#!' class='text-reset'></a>
+                  </p>
+                </div>
+                <div class='col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4'>
+                  <h6 class='text-uppercase fw-bold mb-4'>Contact</h6>
+                  <p>
+                    <i class='fas fa-home me-3'></i> New York, NY 10012, US
+                  </p>
+                  <p>
+                    <i class='fas fa-envelope me-3'></i>
+                    info@example.com
+                  </p>
+                  <p>
+                    <i class='fas fa-phone me-3'></i> + 01 234 567 88
+                  </p>
+                  <p>
+                    <i class='fas fa-print me-3'></i> + 01 234 567 89
+                  </p>
+                </div>
+              </div>
+            </div>
+          </section>
+          <div class='text-center p-4 text-dark'>
+            © 2022 Copyright Shareapremy
+          </div>
+        </footer>
       </section>
     </>
   );
